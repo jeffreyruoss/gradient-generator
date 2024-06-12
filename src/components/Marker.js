559e55c42b2554ad4389208b783f1e0462297bcb
Jeff Ruoss <jeffreyruoss@gmail.com>
2024-06-12 12:04:47 -0700

    Improve design

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,13 +1,14 @@
 import { createTrashIcon } from './TrashIcon.js';
-
-const trashIcon = createTrashIcon();
+import { createPencilIcon } from './PencilIcon.js';
 
 export function createMarker(position, color, index) {
 	return `
     <div class="marker" style="left: ${position};" data-color-value="${color}" data-stop-index="${index + 1}">
-			${trashIcon}
+			${createTrashIcon()}
 			<input type="text" data-coloris class="coloris-picker">
-			<div class="color-swatch" id="color-swatch" style="background-color: ${color}"></div>
+			<div class="color-swatch" id="color-swatch" style="background-color: ${color}">
+				${createPencilIcon()}
+			</div>
     </div>
   `;
 }
