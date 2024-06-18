f8ec8a37574698d77dadc4b4c65e3278ba1dc1dc
Jeff Ruoss <jeffreyruoss@gmail.com>
2024-06-18 16:22:05 -0700

    Refactor import

--- src/components/SavedGradientsSection/import.js
+++ src/components/SavedGradientsSection/import.js
@@ -1,26 +1,40 @@
 export function importSavedGradientsHandler() {
-	importSavedGradientsConfirmation();
+	if (confirmImport()) {
+		handleFileSelection();
+	}
 }
 
-function importSavedGradients() {
-	const file = this.files[0];
+function confirmImport() {
+	const message = 'Are you sure you want to import saved gradients? This will overwrite the current saved gradients.';
+	return confirm(message);
+}
+
+function handleFileSelection() {
+	const importSavedGradientsInput = document.createElement('input');
+	importSavedGradientsInput.type = 'file';
+	importSavedGradientsInput.accept = '.json';
+	importSavedGradientsInput.addEventListener('change', handleFileRead);
+	importSavedGradientsInput.click();
+}
+
+function handleFileRead(event) {
+	const file = event.target.files[0];
 	const reader = new FileReader();
-	reader.onload = function () {
-		const savedGradientsArray = JSON.parse(reader.result);
-		localStorage.setItem('gradient_generator_saved_gradients', JSON.stringify(savedGradientsArray));
-		window.location.reload();
-	}
+	reader.onload = handleFileLoad;
+	reader.onerror = handleFileError;
 	reader.readAsText(file);
 }
 
-function importSavedGradientsConfirmation() {
-	const message = 'Are you sure you want to import saved gradients? This will overwrite the current saved gradients.';
-	const confirmImport = confirm(message);
-	if (confirmImport) {
-		const importSavedGradientsInput = document.createElement('input');
-		importSavedGradientsInput.type = 'file';
-		importSavedGradientsInput.accept = '.json';
-		importSavedGradientsInput.addEventListener('change', importSavedGradients);
-		importSavedGradientsInput.click();
+function handleFileLoad(event) {
+	try {
+		const savedGradientsArray = JSON.parse(event.target.result);
+		localStorage.setItem('gradient_generator_saved_gradients', JSON.stringify(savedGradientsArray));
+		window.location.reload();
+	} catch (error) {
+		console.error('Error parsing JSON', error);
 	}
+}
+
+function handleFileError(event) {
+	console.error('Error reading file', event.target.error);
 }
