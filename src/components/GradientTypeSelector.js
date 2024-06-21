9b190917e4ead205e460e2e5425fcfc91779c966
Jeff Ruoss <jeffreyruoss@gmail.com>
2024-06-21 13:14:45 -0700

    Refactor gradient type selection.

--- src/components/GradientTypeSelector.js
+++ src/components/GradientTypeSelector.js
@@ -2,20 +2,20 @@ import { updateGradient } from "./GradientContainer";
 
 export function createGradientTypeSelector() {
 	return `
-		<div class="gradient-type-selector">
-			<div class="gradient-type-button gradient-type-button-checked">
-				<input type="radio" id="linear" name="gradientType" value="linear" checked>
-				<label for="linear">Linear</label>
-			</div>
-			<div class="gradient-type-button">
-				<input type="radio" id="radial" name="gradientType" value="radial">
-				<label for="radial">Radial</label>
-			</div>
-			<div class="gradient-type-select-degrees">
-				<input type="text" id="degrees" name="degrees" value="" placeholder="0">
-			</div>
-		</div>
-	`;
+        <div class="gradient-type-selector">
+            <div class="gradient-type-button gradient-type-button-checked">
+                <input type="radio" id="linear" name="gradientType" value="linear" checked>
+                <label for="linear">Linear</label>
+            </div>
+            <div class="gradient-type-button">
+                <input type="radio" id="radial" name="gradientType" value="radial">
+                <label for="radial">Radial</label>
+            </div>
+            <div class="gradient-type-select-degrees">
+                <input type="text" id="degrees" name="degrees" value="" placeholder="0">
+            </div>
+        </div>
+    `;
 }
 
 export let gradientType = 'linear';
@@ -28,38 +28,46 @@ let gradientDegreesInput;
 export function initGradientTypeSelector() {
 	const gradientTypeSelector = document.querySelector('.gradient-type-selector');
 	gradientTypeButtons = gradientTypeSelector.querySelectorAll('.gradient-type-button');
-	gradientTypeSelector.addEventListener('change', handleGradientTypeChange);
-
 	gradientDegreesParent = document.querySelector('.gradient-type-select-degrees');
-
 	gradientDegreesInput = document.querySelector('#degrees');
+
+	addEventListeners();
+}
+
+function addEventListeners() {
+	gradientTypeButtons.forEach(button => button.addEventListener('change', handleGradientTypeChange));
 	gradientDegreesInput.addEventListener('keyup', handleDegreesChange);
 }
 
 function handleGradientTypeChange(event) {
-	if (!event.target.parentNode.classList.contains('gradient-type-select-degrees')) {
-		gradientTypeButtons.forEach(button => {
-			button.classList.remove('gradient-type-button-checked');
-		});
-		event.target.parentNode.classList.add('gradient-type-button-checked');
-	}
+	toggleButtonChecked(event);
+	toggleDegreesVisibility(event);
+	gradientType = event.target.value;
+	updateGradient();
+}
 
+function toggleButtonChecked(event) {
+	gradientTypeButtons.forEach(button => button.classList.remove('gradient-type-button-checked'));
+	event.target.parentNode.classList.add('gradient-type-button-checked');
+}
+
+function toggleDegreesVisibility(event) {
 	if (event.target.value === 'radial') {
 		gradientDegreesParent.classList.add('hide');
 	} else {
 		gradientDegreesParent.classList.remove('hide');
 	}
-
-	gradientType = event.target.value;
-	updateGradient();
 }
 
 function handleDegreesChange(event) {
-	if (isNaN(event.target.value) || event.target.value < 0 || event.target.value > 360 || event.target.value === '') {
-		gradientDegrees = '0';
-		gradientDegreesInput.value = '';
-	} else {
-		gradientDegrees = event.target.value;
-	}
+	gradientDegrees = validateDegrees(event.target.value);
 	updateGradient();
+}
+
+function validateDegrees(degrees) {
+	if (isNaN(degrees) || degrees < 0 || degrees > 360 || degrees === '') {
+		gradientDegreesInput.value = '';
+		return '0';
+	}
+	return degrees;
 }
