93704288b471cd8496d32ae4fb89039d743220e0
Jeff Ruoss <jeffreyruoss@gmail.com>
2024-07-03 22:02:03 -0700

    Remove the "no mobile support" banner.

--- src/lib/init.js
+++ src/lib/init.js
@@ -6,14 +6,12 @@ import { initTrashButtons } from '../components/TrashIcon.js';
 import { saveGradientsInit } from '../components/SavedGradientsSection/SavedGradientsSection.js';
 import { initPercentInput } from '../components/Marker/PercentInput.js';
 import { initGradientTypeSelector } from '../components/GradientTypeSelector.js';
-import { noSupportMobile } from './no-support-mobile.js';
 import { initCodeBox } from '../components/CodeBox.js';
 import { initTutorial } from '../components/Tutorial.js';
 
 let gradientRectangle;
 
 export function init() {
-	noSupportMobile();
 	initGradientTypeSelector();
 	initGradientRectangle();
 	gradientRectangle = getGradientRectangle();
