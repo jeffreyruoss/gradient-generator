b13e3e8068b39ef20e604e4a0e7bba4e48076a19
Jeff Ruoss <jeffreyruoss@gmail.com>
2024-07-19 12:54:56 -0700

    Organize.

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-import './style.css'
-import './css/main.css'
-import './css/gradient-editor.css'
-import './css/saved-gradients.css'
-import './css/code-box.css'
-import './src/vendor/coloris.min.css'
-import './src/vendor/coloris.min.js'
-import { init } from './src/lib/init.js';
-import { createGradientContainer } from './src/components/GradientContainer.js'
-import { createSavedGradients } from './src/components/SavedGradientsSection/SavedGradientsSection.js'
-import { createCodeBox } from './src/components/CodeBox.js'
+import { GradientGenerator } from "./GradientGenerator";
 
-document.querySelector('#app').innerHTML = `
-  <div class="wrap">
-    <div class="main">
-      <h1>Gradient Generator</h1>
-      ${createGradientContainer()}
-      ${createCodeBox()}
-      ${createSavedGradients()}
-    </div >
-  </div >
-  `;
+const gradientGenerator = new GradientGenerator('gradient-generator');
 
-document.addEventListener('DOMContentLoaded', () => init());
+gradientGenerator.render();
